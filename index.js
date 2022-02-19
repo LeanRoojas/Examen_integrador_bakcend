3cb076695f0843f2975a1637b0e0f99a748ca120
Leandro Rojas <leandrorojas.educacion@gmail.com>
2022-02-19 02:15:57 -0300

    Agrego validaciones de tokens

--- index.js
+++ index.js
@@ -6,10 +6,17 @@ const bodyParser = require('body-parser');
 const bookRouter = require('./routes/bookRoutes')(Book);
 const userRouter = require('./routes/userRoutes')(User);
 const app = express();
+const jwt = require('express-jwt');
 
 mongoose.connect('mongodb://127.0.0.1:27017/usersAPI');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.all('/*', jwt({
+  secret: 'ultraSecreto',
+  algorithms: ['HS256'],
+}).unless({
+  path: ['/users/login'],
+}));
 app.use('/', userRouter, bookRouter);
 app.listen(8080);
